ab2e2618a07bb47ffc4088774192567c74edff31
Inukaze <inukaze@hotmail.com>
2019-06-12 19:13:27 -0400

    Lista de videojuegos en msdos para usar con dosbox
    ¿Cuales seran los mejores videojuegos para msdos en general para todo el planeta?

---
+++ Lista/videojuegos_para_dosbox.bat
@@ -0,0 +1,364 @@
+cls
+echo NOTA : Estos titulos estan en revision para ver cuales son los mejores segun
+echo Todo el planeta. los que ya tienen "Lanzador" es porque ya los probe
+echo y funcionan perfectamente, pero quiero separar la lista entre los videojuegos
+echo que no requieren CD y los que si.
+echo.
+echo Mejor les digo a : Kokuki de la pocion roja, a jarm, a retrocabeza y al
+echo tuberviejuner, y a slobulus, para crear una encuesta con todos estos titulos y otros
+echo que ellos quieran incluir. para al final ver cuales excluyo.
+echo.
+pause
+cls
+echo *******************************************************************************
+echo * Titulo / Nombre del Videojuego                              *   Lanzador    *
+echo *-------------------------------------------------------------*---------------*
+echo * Abuse
+echo * Action Supercross
+echo * Al-qadim: the genie's curse
+echo * Albion
+echo * Alien Carnage / Halloween Harry
+echo * Alien Rampage (1996)
+echo * Alone in the dark
+echo * Another World / Out of this world
+echo * Baryon
+echo * Battle Chess                                                * bchess.bat    *
+echo * Beneath a steel sky
+echo * Betrayal at krondor
+echo * Bioforge
+echo * Black Throne
+echo * Bram Stoker's Dracula
+echo * Broken Sword : La leyenda de los Templarios (Requiere CD)   * brkswd1.bat   *
+echo * Brutal Sports Football
+echo * Bud Tucker In Double Trouble                                * budtidt.bat   *
+echo * Busy People Of Hamsterland (Requiere Windows 3.1)
+echo * Caesar II
+echo * Cannon Fodder
+echo * Carmaggeddon
+echo * CD-Man (Clon de Pacman)                                     * cdman.bat     *
+echo * Civilization
+echo * Commanche 3
+echo * Command & Conquer
+echo * Commander Keen 4 : Goodbye To Galaxy
+echo * Crusader : No Remorse
+echo * Crystal Caves                                               * ccaves.bat    *
+echo * Dark Seed
+echo * Death Rally
+echo * Descent
+echo * Descent 2
+echo * Destruction Derby 1
+echo * Destruction Derby 2
+echo * Discworld
+echo * Discworld 2
+echo * Disney's : Aladdin                                            * aladdin.bat   *
+echo * Disney's : The Jungle Book                                    * tjbook.bat    *
+echo * Disney's : The Lion King                                      * tlking.bat    *
+echo * Doom II : Hell On Earth                                     * doom2.bat     *                                  
+echo * Doom Ultimate                                               * doom1.bat     *
+echo * Drascula
+echo * Duelo de Hechiceros / Magic Mayhem
+echo * Duke Nukem 3D Atomic Edition                                * dn3dae.bat    *
+echo * Dune 2
+echo * Dungeon Keeper
+echo * Dungeon Master 2
+echo * Earthworm Jim
+echo * Earthworm Jim 2
+echo * Ecstatica
+echo * Ecstatica 2
+echo * Elecman
+echo * Electronic People
+echo * Elvira : Mistress Of The Dark
+echo * En cual parte del mundo esta : Carmen Sandiego?             * carmen.bat    *
+echo * EOJJEONJI JOHEUN
+echo * Epic Pinball (The Complete Edition)
+echo * Exhumed / PowerSlave (Requiere CD)                          * exhumed.bat   *
+echo * Extreme Pinball
+echo * Eye Of Beholder 2 : The Legend Of The Dark Moon
+echo * F-29 Retaliator
+echo * Fade to black
+echo * Fatal Racing / Whiplash
+echo * Flashback : The Quest For Identity
+echo * Frontier : Elite II
+echo * Full Throttle
+echo * FX Fighter
+echo * Gabriel Knight : Sins of the father
+echo * Gobliiins
+echo * Grand Theft Auto
+echo * Grand Theft Auto 2
+echo * GT Racing 97
+echo * Gunship 2000
+echo * H.u.r.l                                                     * hurl.bat      *
+echo * Harvester
+echo * Heretic                                                     * heretic.bat   *
+echo * Heroes Of Might & Magic 2
+echo * Hexen : Beyond Heretic                                      * hexen.bat     *
+echo * Hi-Octane
+echo * I Have No Mouth and I must scream
+echo * Ignition 2
+echo * Indiana Jones and the fate of atlantis
+echo * Ishar (1992)
+echo * Ishar 2 (1993)
+echo * Ishar 3 (1994)
+echo * Jazz Jackrabbit
+echo * Jill Of The Jungle
+echo * Jim Power
+echo * Karateka
+echo * King's Quest V
+echo * Lamborghini: American Challenge
+echo * Land Of Lore 1 : The Throne Of Chaos
+echo * Land Of Lore 2 : Guardians Of Destiny
+echo * Land Of Lore 3 : 
+echo * Larry 7 : Love For Sail (Requiere CD)                       * larry7.bat    *
+echo * Lemmings 1
+echo * Lemmings 2
+echo * Lemmings 3
+echo * LHX Attack Chopper
+echo * LIGHTHOUSE: The Darl Being
+echo * Links 386 PRO
+echo * Little Big Adventure 1 : Restless                           * lba1.bat      *
+echo * Little Big Adventure 2 : Twinsen's Oddysey                  * lba2.bat      *
+echo * Lode Runner : The Legend Returns
+echo * Lotus III : The Ultimate Challange
+echo * LucasArts : Outlaws
+echo * Magic Carpet
+echo * Maniac Mansion                                              * maniac1.bat   *
+echo * Maniac Mansion 2 / Day Of Tentacle                          * mm2dot.bat    *
+echo * Master Of Orion
+echo * Master Of Orion 2
+echo * Mechwarrior 2: 31st century combat aptiva
+echo * Megarace
+echo * Mission Deliver Kindness / MDK                              * mdk.bat       *
+echo * Monkey Island : LeChuck's Revenge
+echo * Mortal Kombat
+echo * Mortal Kombat 3
+echo * Mortal Kombat Trilogy
+echo * Nascar Racing
+echo * Need For Speed II Special Edition                           * nfs2se.bat    *
+echo * Oddworld: Abe's Oddysee                                     * oddw_ao.bat   *
+echo * Operation Carnage
+echo * PC Futbol 5.0
+echo * PC Rally                                                    * pcrally.bat   *
+echo * Phantasmagoria
+echo * PITFall : The Mayan Adventure
+echo * Pray For Death
+echo * Prehistorik
+echo * Prince Of Persia                                            * prince.bat    *
+echo * Prince Of Persia 2 : The Shadow and the Flame               * prince2.bat   *
+echo * Quake
+echo * Raptor: Call of the Shadows
+echo * Rayman (Requiere CD)                                        * rayman.bat    *
+echo * Redneck Rampage
+echo * Rise Of The Robots 2 : Resurrection
+echo * Rise of the Triad (1994)
+echo * Risky Woods
+echo * Saga Ishar: Crystals of Arborea (1990)
+echo * Sam & Max : Hit The Road
+echo * Sango Fighter
+echo * Screamer (Rally)
+echo * Screamer (Rally) 2
+echo * Sega : Outrun                                               * outrun.bat    *
+echo * Shadow Of The Comet
+echo * Shadowcaster
+echo * SHUIHUZHUAN: Kiangshan Yingxiong
+echo * SimCity 2000
+echo * Simon The Sorcerer
+echo * Simon The Sorcerer 2
+echo * Skyroads
+echo * Speedball 2
+echo * Star Control
+echo * Star Control 2
+echo * Star Wars : Dark Forces
+echo * Star Wars : Tie Fighter
+echo * Star Wars : X-Wing Vs Tie Fighter
+echo * StarGunner
+echo * Stonekeep
+echo * Street Fighter 2
+echo * Strife : Trust No One
+echo * Super Ball 2                                                * sb2.bat       *
+echo * System Shock
+echo * Terminal Velocity
+echo * The 7th Guest
+echo * The Castle of Dr. Brain
+echo * The Dig
+echo * The Elder Scroll : Arena
+echo * The Elder Scroll II : Daggerfall (Requiere CD)              * tes2df.bat    *
+echo * The Eye Of Beholder
+echo * The Incredible Machine                                      * tim.bat       *
+echo * The Incredible Machine 2
+echo * The Last Express
+echo * The Lost Vikings
+echo * The Need For Speed                                          * nfs1.bat      *
+echo * The Secret Of Monkey Island
+echo * Tomb Raider                                                 * tombr1.bat    *
+echo * Turrican 2 : The Final Fight
+echo * Tyrian
+echo * UFO : Enemy Unknow
+echo * Ultima IV
+echo * Ultima Underworld: The Stygian Abyss 
+echo * Ultima V
+echo * Ultima VI
+echo * Ultima VII
+echo * Ultima VII : Serpent
+echo * Ultima VII : The Black Gate
+echo * Ultima VIII : Pagan
+echo * Veil Of Darkness
+echo * Wacky Wheels
+echo * Warcraft : Orcs & Humans                                    * warc1.bat     *
+echo * Warcraft 2 : Tides Of Darkness + Beyond The Dark Portal     * warc2-bat     *
+echo * Waxworks
+echo * Wing Commander
+echo * Witchaven                                                   * whaven.bat    *
+echo * Wizardy V
+echo * Wizardy VII : Crusaders of the dark savant
+echo * Wolfenstein 3D                                              * wolf3d.bat    *
+echo * Worms
+echo * X-Com : Terror From The Deep
+echo * X-Men : Children Of The Atom
+echo * Xenon 1
+echo * Xenon 2
+echo * Xenon 2 Megablast
+echo * Zyclunt
+echo *******************************************************************************
+echo * Juegos Españoles (Fuente -> https://www.devuego.es/bd/plataforma/ms-dos//mosaico):
+echo * 25 
+echo * 3 Skulls of the Toltecs 
+echo * 666 El Día del Portal 
+echo * ACCEsinato 
+echo * African Trail Simulator 
+echo * After the War 
+echo * Amo del mundo 
+echo * Ángel Nieto Pole 500 
+echo * Arctic Moves 
+echo * Army Moves 
+echo * Aspar GP Master 
+echo * Autocrash 
+echo * Black Crown 
+echo * Bolix 
+echo * Buggy Ranger 
+echo * Carlos Sainz - Campeonato del Mundo de Rallies 
+echo * Caso Cerrado I: La Mansión 
+echo * Caso Cerrado II: El Pueblo 
+echo * Chess Housers 
+echo * Chichén Itzá 
+echo * Choy-Lee-Fut Kung-Fu Warrior 
+echo * Corsarios 
+echo * Cosmic Sheriff 
+echo * Cozumel 
+echo * Cyber Police 
+echo * Danger World - Capítulo I: Shivani, Ciudad del mal 
+echo * Desperado 2 
+echo * Dimensión Omega 
+echo * Don Quijote 
+echo * Drakkar 
+echo * Dráscula 
+echo * Drazen Petrovic Basket 
+echo * Eclipse Total 
+echo * Eclipse Total 2: Claustrofobia 
+echo * El Capitán Trueno 
+echo * Emilio Butragueño ¡Fútbol! 
+echo * Emilio Sánchez Vicario Grand Slam 
+echo * Enchanted 
+echo * F-1 
+echo * Fernando Martín Basket Master 
+echo * Freddy Hardest 
+echo * Freddy Hardest en Manhattan Sur 
+echo * Game Over 
+echo * Genghis Khan 
+echo * Gonzzalezz 
+echo * Goody 
+echo * Hammer Boy 
+echo * Hoz Forever 
+echo * Hoz Quest 
+echo * Hoz Quest 2: The Gallo's Mission 
+echo * Igor: Objetivo Uikokahonia 
+echo * Jabato 
+echo * Jackson City 
+echo * Jump 
+echo * Ke Rulen los Petas 
+echo * Komebumb 
+echo * Kong's Revenge 
+echo * Kuwait Assault 
+echo * La abadía del crimen 
+echo * La Aventura Espacial 
+echo * La aventura original 
+echo * La Espada Sagrada 
+echo * Leyendas de Lhodrye: Arakhas el Oscuro 
+echo * Leyendas de Lhodrye: La Luz del Druida 
+echo * Livingstone Supongo 
+echo * Livingstone Supongo II 
+echo * Lloni be Good 
+echo * Lorna 
+echo * Los Inhumanos 
+echo * Los Templos Sagrados 
+echo * Luigi & Spaghetti 
+echo * Luigi en Circusland 
+echo * Mad Mix 2: En el Castillo de los Fantasmas 
+echo * Mad Mix Game 
+echo * Mambo 
+echo * Megacorp 
+echo * Megaphoenix 
+echo * Míchel Fútbol Master + Super Skills 
+echo * Mot 
+echo * Mountain Bike Racer 
+echo * Mundial de Fútbol 
+echo * Mutan Zone 
+echo * Narco Police 
+echo * Navy Moves 
+echo * Oberon 69 
+echo * Olimpiadas 92: Atletismo 
+echo * Olimpiadas 92: Gimnasia Deportiva 
+echo * Ormuz 
+echo * París-Dakar 
+echo * PC Basket 
+echo * PC Basket 2.0 
+echo * PC Basket 3.0 
+echo * PC Basket 4.0 
+echo * PC Basket 4.5 
+echo * PC Calcio 
+echo * PC Calcio 4.0 
+echo * PC Fútbol 
+echo * PC Fútbol 3.0 
+echo * PC Fútbol 4.0 
+echo * PC Fútbol 5.0 
+echo * PC Fútbol Argentina 
+echo * PC Fútbol Argentina 4.0 
+echo * PC Selección Española de Fútbol 
+echo * Perico Delgado Maillot Amarillo 
+echo * Phantis 
+echo * Piso Zero 
+echo * R.A.M 
+echo * Rath-tha 
+echo * Risky Woods 
+echo * Rock'n Roller 
+echo * Rol Crusaders 
+echo * Satan 
+echo * Sila's Quest 
+echo * Silent Shadow 
+echo * Simulador Profesional de Fútbol 
+echo * Simulador Profesional de Tenis 
+echo * Sito Pons 500c.c. Grand Prix 
+echo * Sol Negro 
+echo * Speed Demons 
+echo * Speed Haste 
+echo * Star Bowls 
+echo * Star Dust 
+echo * Stroper 
+echo * Supercyber 
+echo * Telemark Warrior 
+echo * The Brick 
+echo * The Castle of Dr. Malvado 
+echo * The Last Mission 
+echo * Throwland 
+echo * Thunder Offshore 
+echo * Tie Break Tenis 98' 
+echo * Titanic 
+echo * Tokenkai 
+echo * Tour 91 
+echo * Trauma 
+echo * Trick or Treat 
+echo * Tuma-7 
+echo * Ulises 
+echo * Viaje al centro de la Tierra 
+echo * World Wide Rally 
+echo * Zona 0 
+echo *******************************************************************************
